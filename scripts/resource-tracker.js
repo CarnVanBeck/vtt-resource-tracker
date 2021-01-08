fc41a84e5539d625e723dedae16a1ccbac4d0f52
CarnVanBeck <CarnVanBeck@gmail.com>
2021-01-08 15:15:32 +0100

    Update and rename simple-stealth.js to resource-tracker.js

--- scripts/resource-tracker.js
+++ scripts/resource-tracker.js
@@ -6,7 +6,6 @@ class simpleStealth {
         if (actor === undefined || actor.data.type === 'npc') {
             return;
         }
-        console.log(`simple-stealth: canvas.tokens.get('${data._id}').actor.data.data.resources;`);
 
         let resProperty;
         let resource;
@@ -24,32 +23,14 @@ class simpleStealth {
             return;
         }
 
-        //let position = game.settings.get('vtt-simple-stealth', SIMPLESTEALTH_POSITION_MODULE_SETTING);
-
-        //<!--<i class="fas fa-dice-d20"></i>--></div>
-
         let stealthInput = $(`<input type="text" name="simple-stealth-value" placeholder="0" value="${stealthVal}" />`);
         let stealthContainer = $(`<div class="simple-stealth-container" title="${game.i18n.localize('DND5E.SkillSte')}"></div>`);
 
-        //let buttons = $(`<div class="control-icon">${stealthButton}</div>`);
         let target = '.col.right';
-        /*
-        switch(position) {
-          case 'left':
-            target = '.col.left';
-            break;
-          case 'right':
-            target = '.col.left';
-             break;
-          default:
-            target = '.col.middle';
-        }
-        */
         stealthContainer.append(stealthInput);
         html.find(target).append(stealthContainer);
 
         stealthInput.focusout(async (e) => {
-            console.log(e.target.value);
             actor.data.data.resources[resProperty].value = e.target.value;
         })
     }
@@ -58,22 +39,3 @@ class simpleStealth {
 Hooks.on('renderTokenHUD', (app, html, data) => {
     simpleStealth.addStealthField(app, html, data)
 });
-
-/*
-Hooks.on('init', function () {
-    game.settings.register('vtt-simple-stealth', SIMPLESTEALTH_POSITION_MODULE_SETTING, {
-        name: game.i18n.localize('settings.fieldPosition'),
-        hint: game.i18n.localize('settings.fieldHint'),
-        scope: 'world',
-        config: true,
-        type: String,
-        default: 'bottom',
-        choices: {
-            'left': game.i18n.localize('settings.position.left'),
-            'right': game.i18n.localize('settings.position.right'),
-            'top': game.i18n.localize('settings.position.top'),
-            'bottom': game.i18n.localize('settings.position.bottom')
-        }
-    });
-});
-*/
