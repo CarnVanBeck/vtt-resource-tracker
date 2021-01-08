5fd62ebc5c90a23f34a08160027e832bd3d2be22
CarnVanBeck <carnvanbeck@gmail.com>
2021-01-09 00:57:56 +0100

    completely overwritten to be a resource tracker instead of simply stealth

--- scripts/resource-tracker.js
+++ scripts/resource-tracker.js
@@ -1,41 +1,193 @@
-const SIMPLESTEALTH_POSITION_MODULE_SETTING = 'position';
+const RESTRACK_MODULENAME = 'vtt-resource-tracker';
+const RESTRACK_POSITION_SETTING = 'tracker-position'
+class resourceTracker {
+    static async addTrackFields(app, html, data) {
+        let position = game.settings.get(RESTRACK_MODULENAME, RESTRACK_POSITION_SETTING);
 
-class simpleStealth {
-    static async addStealthField(app, html, data) {
+        let entity = app.object;
+
         let actor = canvas.tokens.get(data._id).actor;
-        if (actor === undefined || actor.data.type === 'npc') {
+        let newdiv = $('<div class="resource-tracker-container">');
+        let recources = $(`<div class="col resource-tracker-column resource-tracker-column-${position}"></div>`);
+        for (const [key, res] of Object.entries(entity.data.flags[RESTRACK_MODULENAME])) {
+            let row = $('<div class="resource-tracker-row"></div>');
+
+
+            let value;
+            let max;
+            let label;
+            let icon;
+
+            if (key.includes('restrack_custom_')) {
+                let res = entity.getFlag(RESTRACK_MODULENAME, key);
+                value = res.value;
+                label = res.label;
+                icon = res.icon;
+            }
+            else {
+                value = actor.data.data.resources[key].value;
+                max = actor.data.data.resources[key].max;
+                label = actor.data.data.resources[key].label;
+            }
+            let resourceInput = $(`<input type="text" data-key="${key}" data-module="${RESTRACK_MODULENAME}" min="0" ${(max?.length > 0 ? 'max = "' + max + '"' : '')} placeholder="0" value="${value ?? ''}" title="${label}" />`);
+
+            if (icon && icon.length > 0) {
+                //check if image exists
+                $.get(icon)
+                    .done(function () {
+                        let resourceIcon = $(`<img src="${icon}" width="36" height="36" title="${label}">`);
+                        row.addClass('resource-tracker-row-withIcon');
+                        resourceInput.before(resourceIcon);
+                    })
+            }
+
+            row.append(resourceInput);
+            recources.append(row);
+        }
+
+        html.find('.col.right').wrap(newdiv);
+        html.find('.col.right').before(recources);
+
+        $(`input[data-module=${RESTRACK_MODULENAME}`).focusout(async (e) => {
+            let key = $(e.target).attr('data-key');
+            let value = $(e.target).val();
+
+            if (key.includes('restrack_custom_')) {
+                entity.data.flags[RESTRACK_MODULENAME][key].value = value;
+            }
+            else {
+                actor.data.data.resources[key].value = value;
+            }
+            //actor.data.data.resources[resProperty].value = e.target.value;
+        })
+    }
+
+    static async injectSettings(entity, html) {
+        if (!entity || !entity.actor || !entity.token) {
             return;
         }
 
-        let resProperty;
-        let resource;
-        let stealthVal = '';
-        for (const property in actor.data.data.resources) {
-            if (actor.data.data.resources[property].label == game.i18n.localize('DND5E.SkillSte')) {
-                resource = actor.data.data.resources[property];
-                stealthVal = resource.value ?? '';
-                resProperty = property;
+        // fix config height
+        html.height("auto");
+        // add element to config screen
+        let additionalHtml = $(`<h3>${game.i18n.localize('ResTrack.moduleName')}</h3><p class="notes">${game.i18n.localize('ResTrack.settings.token.trackResourceHint')}</p>`);
+        html.find(`.tab[data-tab="resources"]`).append(additionalHtml);
+
+        //add resources from actor (only for PCs)
+        if (entity.actor.data.type == 'character') {
+            for (const [key, res] of Object.entries(entity.actor.data.data.resources)) {
+                resourceTracker.appendTrackField(html, entity, key, res);
             }
         }
-
-        if (resource === undefined) {
-            ui.notifications.warn(game.i18n.localize('notification.noStealthAttribute') + game.i18n.localize('DND5E.SkillSte'));
-            return;
+        //add custom resources from token
+        for (const [key, res] of Object.entries(entity.token.data.flags[RESTRACK_MODULENAME])) {
+            resourceTracker.appendTrackField(html, entity, key, res);
         }
 
-        let stealthInput = $(`<input type="text" name="simple-stealth-value" placeholder="0" value="${stealthVal}" />`);
-        let stealthContainer = $(`<div class="simple-stealth-container" title="${game.i18n.localize('DND5E.SkillSte')}"></div>`);
+        //create custom resource tracking
+        html.find(`.tab[data-tab="resources"]`).append(`<hr /><p class="notes">${game.i18n.localize('ResTrack.settings.token.addCustom')}<i id="restrack_addCustom" class="fas fa-plus"></i></p>`);
 
-        let target = '.col.right';
-        stealthContainer.append(stealthInput);
-        html.find(target).append(stealthContainer);
+        //handle add custom resource
+        html.find('[id="restrack_addCustom"]').on('click', (e) => {
+            let nextCustom = 0;
+            //find the next free number
+            for (const [key, res] of Object.entries(entity.token.data.flags[RESTRACK_MODULENAME])) {
+                if (key.includes('restrack_custom_')) {
+                    if (key.replace('restrack_custom_', '') == nextCustom) {
+                        nextCustom++;
+                    }
+                    else {
+                        break;
+                    }
+                }
+            }
 
-        stealthInput.focusout(async (e) => {
-            actor.data.data.resources[resProperty].value = e.target.value;
-        })
+            resourceTracker.appendTrackField(html, entity, `restrack_custom_${nextCustom}`, null);
+        });
+
+        // handle submit
+        html.find('button[type=submit]').on('click', () => {
+            html.find($('input[id^="restrack_resource_"]')).each(function () {
+                let key = $(this).attr('data-name');
+
+                if (this.checked) {
+                    let res = {
+                        'tracked': true
+                    };
+
+                    if (key.includes('restrack_custom_')) {
+                        res.label = html.find(`input[data-name="${key}"]`).val();
+                        res.value = null;
+                        res.isCustom = true;
+                        res.icon = html.find(`input[data-name="${key + '_icon'}"]`).val();
+                    }
+                    entity.token.setFlag(RESTRACK_MODULENAME, key, res);
+                }
+                else {
+                    entity.token.unsetFlag(RESTRACK_MODULENAME, key);
+                }
+            });
+        });
+    }
+
+    /** @private */
+    static async appendTrackField(html, entity, key, res) {
+        let resourceContainer = $('<div class="form-group"></div>');
+
+        let localizedName = game.i18n.localize('DND5E.Resource' + key.titleCase());
+        //fallback if a resource is used, that has no localized name
+        if (localizedName.includes('DND5E.Resource')) {
+            localizedName = key;
+        }
+
+        let checked = entity.token.getFlag(RESTRACK_MODULENAME, key)?.tracked ?? false;
+
+        let resourceLabel;
+        if (key.includes('restrack_custom_')) {
+            let customLabel = entity.token.getFlag(RESTRACK_MODULENAME, key)?.label ?? '';
+            let resourceName = `<input type="text" data-name="${key}" placeholder="${game.i18n.localize('ResTrack.settings.token.addCustomPlaceholder')}" value="${customLabel}"/>`;
+            let resourceIcon = `<input type="text" data-name="${key + '_icon'}" placeholder="${game.i18n.localize('ResTrack.settings.token.addCustomIconPlaceholder')}" value="${res.icon}" />`;
+
+            resourceLabel = $(resourceName + resourceIcon);
+        }
+        else {
+            resourceLabel = $(`<label>${localizedName + (res && res.label && res.label.length > 0 ? ` (${res.label})` : '')}</label>`);
+        }
+
+        let resourceFormfield = $('<div class="form-fields"></div>');
+        let resourceCheckbox = $(`<input type="checkbox" id="restrack_resource_${key}" data-name="${key}" ${checked ? 'checked' : ''} />`);
+        resourceFormfield.append(resourceCheckbox);
+        resourceContainer.append(resourceLabel).append(resourceFormfield);
+        html.find(`.tab[data-tab="resources"]`).append(resourceContainer);
     }
 }
 
 Hooks.on('renderTokenHUD', (app, html, data) => {
-    simpleStealth.addStealthField(app, html, data)
+    console.log(`${RESTRACK_MODULENAME} | Hook on renderTokenHUD`);
+    resourceTracker.addTrackFields(app, html, data);
+});
+
+Hooks.on(
+    "renderTokenConfig",
+    function (entity, html) {
+        console.log(`${RESTRACK_MODULENAME} | Hook on renderTokenConfig`);
+        resourceTracker.injectSettings(entity, html);
+    }
+);
+
+Hooks.on('init', () => {
+    console.log(`${RESTRACK_MODULENAME} | Hook on init`);
+
+    game.settings.register(RESTRACK_MODULENAME, RESTRACK_POSITION_SETTING, {
+        name: game.i18n.localize('ResTrack.settings.trackerPosition'),
+        hint: game.i18n.localize('ResTrack.settings.trackerPositionHint'),
+        scope: 'client',
+        config: true,
+        type: String,
+        default: 'right',
+        choices: {
+            'left': game.i18n.localize('ResTrack.settings.trackerPositionLeft'),
+            'right': game.i18n.localize('ResTrack.settings.trackerPositionRight'),
+        }
+    });
 });
